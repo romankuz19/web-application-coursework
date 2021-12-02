8b18c679ba042395a577b973d9a69a142515c051
romankuz19 <80103795+romankuz19@users.noreply.github.com>
2021-12-02 16:12:46 +0300

    Delete htdocs directory

--- htdocs/exit.php
+++
@@ -1,8 +0,0 @@
-<?php
-setcookie('user', $user['name'],time() - 3600, "/");
-require 'configDB.php';
-
-$query1 = $pdo->query("UPDATE `users` SET `online`= 0 WHERE `online` = 1");
-header('Location: /');
-
-?>
